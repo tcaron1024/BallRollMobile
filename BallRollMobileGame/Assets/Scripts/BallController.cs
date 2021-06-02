a83fdbda38091fbedf0bb7412496f84300ce06e5
IcarusPanda <45722136+IcarusPanda@users.noreply.github.com>
2021-06-01 22:22:45 -0500

    Foward momentum and speed indicator
    I added in that if you swipe forward you will start moving faster. It's a pretty rough prototype

--- BallRollMobileGame/Assets/Scripts/BallController.cs
+++ BallRollMobileGame/Assets/Scripts/BallController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
@@ -14,11 +15,15 @@ public class BallController : MonoBehaviour
     public float amountDivisor = 500f;
 
     [SerializeField] private float minMaxSpeedDiff = 4f;
-    private float maxSpeed;
-    private float minSpeed;
+    private float maxSpeed = 50.0f;
+    private float minSpeed = 0.0f;
     private float yTarget = -.2f;
 
     private Vector2[] touchStartPositions = new Vector2[10];
+
+    //Shows the player how fast they are going
+    public Slider speedReference;
+
 
     void Start()
     {
@@ -28,6 +33,12 @@ public class BallController : MonoBehaviour
         print(Vector3.forward * startForce);
         rb.AddForce(transform.forward * startForce);
         //Invoke("GetVelocityNormalized", 2f);
+
+        //Setting up values for speed refernce
+        speedReference.value = ballSpeed;
+        speedReference.maxValue = maxSpeed;
+        speedReference.minValue = minSpeed;
+
     }
 
     void Update()
@@ -40,7 +51,7 @@ public class BallController : MonoBehaviour
 
     private void ForwardMovement()
     {
-        rb.AddForce(GameController.pathSlope * GameController.gameSpeed * ballSpeed * Time.deltaTime);
+        rb.AddForce(GameController.pathSlope * GameController.gameSpeed * ballSpeed * Time.deltaTime);   
         //print("adding force - " + GameController.pathSlope * GameController.gameSpeed * ballSpeed * Time.deltaTime);
     }
 
@@ -55,7 +66,26 @@ public class BallController : MonoBehaviour
             else if (Input.touches[i].phase == TouchPhase.Ended || Input.touches[i].phase == TouchPhase.Canceled)
             {
                 Vector2 direction = Input.touches[i].position - touchStartPositions[i];
+                Debug.Log("direction.X = " + direction.x);
+
+                /*If ball is going under max speed, and the player has swiped
+               upwards, increase ball speed*/
+                if(ballSpeed < maxSpeed)
+                {
+                    ballSpeed += (direction.y % 100) / 10;
+                    Debug.Log("ball speed = " + ballSpeed);
+                }
+                else
+                {
+                    //Only if the input is negative, add ball speed
+                    if(direction.y < minSpeed)
+                    {
+                        ballSpeed += (direction.y % 100) / 10;
+                    }
+                }
+                speedReference.value = ballSpeed;
                 MoveBall(direction.x, direction.magnitude);
+
             }
         }
 
