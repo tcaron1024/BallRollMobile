4927c028ecfecd4fa684a70fc8555736fc4de036
Kyle Grenier <kyle.grenier11@gmail.com>
2021-06-04 19:49:50 -0500

    Added a gitignore, changed camera movement to using LateUpdate instead of Update

--- BallRollMobileGame/Assets/Scripts/CamFollower.cs
+++ BallRollMobileGame/Assets/Scripts/CamFollower.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,7 +11,7 @@ public class CamFollower : MonoBehaviour
     private Vector3 target;
     
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         camSpeed = GameController.gameSpeed;
         target = ball.position + offset;
