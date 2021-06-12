631b4057d56b3a1d4b299de1b4f3294af6415986
Kyle Grenier <kyle.grenier11@gmail.com>
2021-06-11 20:23:40 -0500

    swipe controls V1
    Amount ball is moved is based on distance of swipe. The further the swipe, the more force will be applied to the ball.
    
    The swipe can take place anywhere on the screen (i.e., you don't have to swipe on the ball).
    It is also possible to start to swipe in a direction, then keep your finger stationary, and the ball will gradually accelerate in that direction.

--- BallRollMobileGame/Assets/Scripts/BallController.cs
+++ BallRollMobileGame/Assets/Scripts/BallController.cs
@@ -5,26 +5,32 @@ using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
-    Rigidbody rb;
+    private Rigidbody rb;
+
+    #region -- Ball Movement Fields --
+    [Header("Ball Movement")]
 
     public float startForce;
     public float ballSpeed = 7f;
-    public float amountDivisor = 500f;
+
+    [Tooltip("A multiplier that affects how fast the ball is travelling.")]
+    [SerializeField] private float speedMultiplier;
 
     [SerializeField] private float minMaxSpeedDiff = 4f;
     private float maxSpeed = 50.0f;
     private float minSpeed = 0.0f;
+    #endregion
+
+    #region -- Swipe Controls --
+    private Vector2 startTouchPosition;
+    private Vector2 currentTouchPosition;
+    private Vector2 endTouchPosition;
+    #endregion
 
     //Shows the player how fast they are going
     public Slider speedReference;
 
     private float levelSpeed;
-
-    // Swipe Controls
-    private Vector2 startTouchPosition;
-    private Vector2 currentTouchPosition;
-    private Vector2 endTouchPosition;
-
 
     void Start()
     {
@@ -69,40 +75,27 @@ public class BallController : MonoBehaviour
         }
 
         // If the player has already begun touching the screen and is now in the process of performing
-        // a swipe.
-        else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        // a swipe, take the touch distance as a force magnitude and apply a force to the ball.
+        else if (Input.touchCount > 0)
         {
             currentTouchPosition = Input.GetTouch(0).position;
 
             // Get the distance between the two touches.
             Vector2 touchDistance = currentTouchPosition - startTouchPosition;
-            print(touchDistance.sqrMagnitude);
+            float forceMagnitude = touchDistance.x;
+            ApplyForceToBall(forceMagnitude);
         }
     }
 
     /// <summary>
     /// Applies a force to the ball. Used to control the ball via swipe controls.
     /// </summary>
-    /// <param name="force">The force to apply to the ball.</param>
-    private void ApplyForceToBall(Vector3 force)
+    /// <param name="forceMagnitude">The magnitude of the force to apply to the ball.
+    /// Force will be applied over the X-axis.</param>
+    private void ApplyForceToBall(float forceMagnitude)
     {
-
-    }
-
-
-    private void MoveBall(float pos, float amount)
-    {
-        if (pos != 0)
-        {
-            amount /= amountDivisor;
-            //print("x pos change = " + pos + " amount = " + amount);
-            rb.AddForce((pos > 0 ? Vector3.right * amount : Vector3.left * amount));
-        }
-        else
-        {
-            //print("swipe didn't register movement");
-        }
-
+        rb.AddForce(Vector3.right * forceMagnitude * speedMultiplier, ForceMode.Force);
+        print(Vector3.right * forceMagnitude * speedMultiplier);
     }
 
     public void SetLevelSpeed(float speed)
