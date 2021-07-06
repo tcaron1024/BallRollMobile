9edf86f683f4ecc8bbaed396412ae879e752b2f5
SpencerStilwell <stilwellspencer@hotmail.com>
2021-07-06 07:21:51 -0500

    Snowball and Icicles
    prefabs added, no functionality.

---
+++ BallRollMobileGame/Assets/Scripts/MarbleTrailParenting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleTrailParenting : MonoBehaviour
+{
+    public GameObject marble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = marble.transform.position;
+    }
+}
