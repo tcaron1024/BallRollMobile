236a202d02788304ea894832c277dd4a3ad2017d
Kyle Grenier <kyle.grenier11@gmail.com>
2021-07-19 22:36:23 -0500

    Made pyramids slightly bouncier via code
    When the player comes into contact with a pyramid, an impulse force is added to the player's marble in the normal direction of the contact.

---
+++ BallRollMobileGame/Assets/Scripts/Obstacles/PyramidBounce.cs
@@ -0,0 +1,26 @@
+/*****************************************************************************
+// File Name :         PyramidBounce.cs
+// Author :            Kyle Grenier
+// Creation Date :     07/19/2021
+//
+// Brief Description : Adds force to the player in the opposite direction they're travelling in 
+                       on impact with the game object.
+*****************************************************************************/
+using UnityEngine;
+
+public class PyramidBounce : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRb = col.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 force = col.GetContact(0).normal * playerRb.velocity.z;
+            
+            playerRb.AddForce(force, ForceMode.Impulse);
+
+            //print(Vector3.Reflect(col.gameObject.GetComponent<Rigidbody>().velocity,col.GetContact(0).normal));
+        }
+    }
+}
