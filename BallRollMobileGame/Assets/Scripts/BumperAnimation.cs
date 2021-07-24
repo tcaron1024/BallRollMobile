4a001fe7d00bfc22af38ad4efb6d33a0de6a1ac5
SpencerStilwell <stilwellspencer@hotmail.com>
2021-07-24 18:25:50 -0500

    Fan and Bumpers added
    fan and bumpers visually added, no functionality.

---
+++ BallRollMobileGame/Assets/Scripts/BumperAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperAnimation : MonoBehaviour
+{
+    Animator anim;
+    public GameObject ringSpawn;
+    Vector3 ringLocation;
+    public GameObject ring;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        ringLocation = new Vector3(ringSpawn.transform.position.x, ringSpawn.transform.position.y, ringSpawn.transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            anim.SetBool("HittingBall", true);
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            anim.SetBool("HittingBall", false);
+        }
+    }
+
+    void SpawnRing()
+    {
+        Instantiate(ring, ringLocation, Quaternion.identity);
+    }
+}
---
+++ BallRollMobileGame/Assets/Scripts/DestroyItself.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItself : MonoBehaviour
+{
+    // This is used for animation events
+    void SelfDestruct()
+    {
+        Destroy(gameObject);
+    }
+}
